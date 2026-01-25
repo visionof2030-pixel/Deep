6d92ebd7d454cc6d1764bedebdd93a550218e764
visionof2030-pixel <visionof2030@gmail.com>
2026-01-26 01:48:00 +0300

    Update security.py

--- security.py
+++ security.py
@@ -1,12 +1,6 @@
 # security.py
-from fastapi import Header, Depends, Request
+from fastapi import Header, Depends
 from key_logic import verify_code
 
-def activation_required(
-    request: Request,
-    x_activation_code: str = Header(...)
-):
-    """
-    اعتماد تحقق من التفعيل مع تسجيل معلومات الطلب
-    """
-    verify_code(x_activation_code, request)
+def activation_required(x_activation_code: str = Header(...)):
+    verify_code(x_activation_code)
